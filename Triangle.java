b4cf9923df380f0ae8f477db001838c0f963dbeb
Shahadil Munawir <84338074+ShahadilMunawir@users.noreply.github.com>
2022-08-29 09:13:58 +0530

    added area calculation

--- Triangle.java
+++ Triangle.java
@@ -1,22 +1,33 @@
 // Program 2
 
 import java.util.Scanner;
+import java.lang.Math;
 
 class Triangle{
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter side x: ");
         int x = input.nextInt();
-        System.out.print("\nEnter side y: ");
+        System.out.print("Enter side y: ");
         int y = input.nextInt();
         System.out.print("Enter side z: ");
         int z = input.nextInt();
+        
         if(x == y && y == z){
             System.out.println("The triangle is equilateral");
-        }else if(x ==y || y == z || x == z){
+        }else if(x == y || y == z || x == z){
             System.out.println("The triangle is isocelus");
         }else{
             System.out.println("The triangle is scalene");
+        }
+        
+        int s = (x+y+z)/2;
+        double area = Math.pow((s*(s-x)*(s-y)*(s-z)), 0.5);
+        if(area == 0){
+            System.out.println("The sum of two side lengths has to exceed the length of the third side");
+        }
+        else{
+            System.out.println("Area: " + area);
         }
     }
 }
